e961a415f502dff42504356075d3b9b28b43a9c1
Cnochur <cnochursaunders@gmail.com>
2023-11-28 17:19:57 +0000

    script file crerated
    external script file created and pseudocode added

---
+++ script.js
@@ -0,0 +1,16 @@
+//--------------------------------Pseudocode----------------------------------\\
+
+//Retrieve input var from player and store in playerChoice var
+//Generate a random choice and store in computersChoice var
+//Compare playersChoice and computerChoice and store in result var
+//Use result var Check if player won or lost, computer won or lost or a tie
+//In case of a tie restart the round
+//If win or lose out put tally score for winner, print winner and restart
+//restart game choice to reset scores to 0
+
+//------------------------------------------------------------------------------\\
+
+
+//Retrieve input var from player and store in playerChoice var
+
+
