981ab537724d7e63ddcf0fe41298e445ee0a2e0d
Cnochur <cnochursaunders@gmail.com>
2023-11-29 14:43:12 +0000

    1st Draft
    Game completed, need to fix a bug where scissors beats rock

--- script.js
+++ script.js
@@ -10,7 +10,42 @@
 
 //------------------------------------------------------------------------------\\
 
+let computerChoice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;;
+let playerChoice = prompt("Rock, Paper or scissors?: ");
 
-//Retrieve input var from player and store in playerChoice var
+switch(playerChoice){
+    case "rock":
+        playerChoice = 1;
+        break;
+    case "paper":
+        playerChoice = 2;
+        break;
+    case "scissors":
+        playerChoice = 3;
+        break;
+}
+
+console.log(`Player chose: ${playerChoice} Computer chose: ${computerChoice}`);
+winner(playerChoice, computerChoice);
+
+// let playAgain = prompt("Another Round? Please type yes or no: ")
+
+// switch
+
+
+
+function winner (a, b){
+    let ply = "Player";
+    let com = "Computer";
+
+    if (a > b){
+        console.log(ply + " WINS!")
+    }
+    else if (a < b){
+        console.log(com + " WINS!")
+    }
+    else{console.log('Tie! Try Again')}
+
+}
 
 
