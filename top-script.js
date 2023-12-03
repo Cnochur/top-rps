6d1d5c648d15c48abef76f255be3438c40ea642b
Cnochur <cnochursaunders@gmail.com>
2023-12-03 12:11:13 +0000

    playRound, game, again
    functions implemented to continue game on request and repeat tie rounds

--- top-script.js
+++ top-script.js
@@ -1,34 +1,32 @@
-let playerChoice = "";
-
-getPlayerSelection();
-getComputerChoice();
 
 
+game();
+again();
 
 
 
 
-
-function getPlayerSelection (){
+function getPlayerChoice(){
     
     let choice = prompt("Rock, Paper or Scissors?");
-    let userChoice = choice; 
+    let final = "";
 
     if (choice.toLowerCase() === "rock"){
-        userChoice = "Rock";
-        console.log(userChoice);
+        final = 1;
+        console.log("Player chose: Rock");
     }
     else if (choice.toLowerCase() === "paper"){
-        userChoice = "Paper";
-        console.log(userChoice);
+        final = 2;
+        console.log("Player chose: Paper");
     }
     else if (choice.toLowerCase() === "scissors"){
-        userChoice = "Scissors";
-        console.log(userChoice);
+        final = 3;
+        console.log("Player chose: Scissors");
     }
     else {
-        userChoicehoice = "ERROR!";
+        choice = "ERROR!";
     }
+    return final;
 }
 
 function getComputerChoice(){
@@ -36,18 +34,81 @@ function getComputerChoice(){
     let ans = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
     if (ans === 1){
-        ans = "Rock";
-        console.log(ans);
+        console.log("Computer chose: Rock");
     }
     else if (ans === 2 ){
-        ans = "Paper";
-        console.log(ans);
+        console.log("Computer chose: Paper");
     }
     else if (ans === 3 ){
-        ans = "Scissors";
-        console.log(ans);
+        console.log("Computer chose: Scissors");
     }
     else {
         ans = "ERROR!";
     }
+
+    return ans;
+}
+
+function playRound(){
+    
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+
+    roundWinner(playerChoice, computerChoice);
+
+}
+
+function roundWinner(a,b){
+    
+    let ply = "Player";
+    let com = "Computer";
+
+
+    if(a === b){
+
+        console.log("Its a TIE, try again!!");
+        playRound();
+
+    }
+    else if ((a === 3) && (b === 1)){
+
+        console.log(com + " WINS!")
+        
+    }
+    else if ((b === 3) && (a === 1)){
+        console.log(ply + " WINS!");
+        
+    }
+    else if (a < b){
+        console.log(com + " WINS!");
+        
+    }
+    else if (a > b){
+        console.log(ply + " WINS!");
+        
+    }
+    else{
+        console.log('ERROR');
+        
+    }
+}
+function game (){
+
+ for (i = 0; i < 1; i++){
+        console.log(`Game: ` + [i + 1]);
+        playRound();
+    }
+
+}
+function again(){
+    let playAgain = prompt("Would you like to play again?");
+    playAgain = playAgain.toLowerCase();
+
+    while(playAgain = "yes"){
+        game();
+        let playAgain = prompt("Would you like to play again?");
+        playAgain.toLowerCase();
+
+    }
+
 }
